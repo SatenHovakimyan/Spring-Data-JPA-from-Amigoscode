ca84eadf29beb78a3d96afb8228ab9c50361116d
SatenHovakimyan <saten.hovakimyan5@gmail.com>
2021-08-19 22:32:17 +0400

    all done from youtube channel

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Example;
+import org.springframework.data.repository.query.Param;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,24 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student Saten = new Student(
+                    "Saten",
+                    "Hovakimyan",
+                    "saten.hovakimyan5@gmail.com",
+                    21
+            );
+            studentRepository.save(Saten);
+            studentRepository.save(new Student(
+                    "Vlad",
+                    "Abramyan",
+                    "vlad.abramyan5@gmail.com",
+                    21
+            ));
+
+
+        };
+    }
 }
